063fb1ba219dc7bae66ce268f449dacb81546999
amakom <obiajuluamakom@gmail.com>
2021-09-20 17:08:35 +0100

    First Commit

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-$('.sponsor-logos').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay:true,
-    autoplaySpeed: 1500,
-    arrows: false,
-    dots: false,
-    pauseOnHover: false,
-responsive: [
-{
-  breakpoint: 768,
-  settings: {
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    infinite: false,
-    dots: false
-  }
-},
-{
-  breakpoint: 520,
-  settings: {
-    slidesToShow: 3,
-    slidesToScroll: 3
-  }
-},
-// You can unslick at a given breakpoint now by adding:
-// settings: "unslick"
-// instead of a settings object
-]
-});
+
+// scrolling effect
 
 jQuery(document).ready(function( $ )
 { $('.counter').counterUp({
@@ -41,3 +13,4 @@ window.addEventListener('scroll', function () {
   let windowPosition = window.scrollY > 700;
   header.classList.toggle('scrolling-active', windowPosition);
 })
+
